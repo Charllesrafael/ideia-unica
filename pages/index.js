151a46a7bac49b959564ba0f091073a3feffbbcf
Charlles Rafael <charllesrafael@hotmail.com>
2021-12-11 14:59:59 -0300

    Primeira pagina 2

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,8 @@ import { useState } from "react"
 function Home(){
   return (
   <div>
-    <h1>Home 123</h1>
-    <Contador />
+    <h1>Home teste</h1>
   </div>
   )
 }
-
-function Contador() {
-  const [contador, serContador] = useState(1);
-
-  function adicionarContador(){
-    serContador(contador + 1);
-  }
-
-  return (
-    <div>
-      <div>{contador}</div>
-      <button onClick={adicionarContador}>Adicionar</button>
-    </div>
-  )
-}
-
 export default Home
